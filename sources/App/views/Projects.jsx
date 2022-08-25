6439da9210a495b3abeaebffcfa25188aa4e7234
Samy <SamyIbrizPelaez.Dev@gmail.com>
2022-08-25 18:44:59 +0200

    Project Section Content
    -Added Knight Tours in windows form and console

--- sources/App/views/Projects.jsx
+++ sources/App/views/Projects.jsx
@@ -9,7 +9,7 @@ projects.push({
     image: "assets/projects/portfolio.png",
     description: "Building your own web portfolio as a developer is the best way to present yourself. Starting with a simple concept is key. \n \n"+
     "It is built as a responsible single page application. The 3D earth crowns the scene while the landing application makes a first contact with the visitor.",
-    technologies: ["Javascript","React", "Three.js", "Anime.js", "ReCaptcha", "HTML5", "CSS3"]
+    technologies: ["HTML5", "CSS3", "Javascript","React", "Three.js", "Anime.js", "ReCaptcha"]
 });
 projects.push({
     id:"2",
@@ -19,7 +19,23 @@ projects.push({
     description: "The user constant need to authentify itself, has motivated the market to produce a whole range of solutions to help the user manage its credetials. \n"+
     "Therefore, most of them require direct access to the internet or a specific device. \n\n Consequently, PasswordCard, brings a software solution to generate password cards " +
     "which can be printed in a credit card (ISO/IEC 7810) and bring to the user a flexible, secure and persistent solution to bring their credentials everywhere in their pockets.",
-    technologies: ["UWP", "MySQL", "PDFSharpCore"],
+    technologies: ["C#", "XAML", "UWP", "MySQL", "PDFSharpCore"],
+});
+projects.push({
+    id:"3",
+    name: "Knight's Tour",
+    catchphrase: "My kingdom for a horse!",
+    image: "assets/projects/knightsTour.png",
+    description: "A videogame in Windows Forms based in the Knight's Tour mechanics. The knight visits every square at least once with out returning a second time. The game is lost when no possible movements are available. It is won when avery square of the board has been visited.",
+    technologies: ["C#", "Windows Form"],
+});
+projects.push({
+    id:"3",
+    name: "Knight's Tour",
+    catchphrase: "My console for a horse!",
+    image: "assets/projects/knightsTourConsole.png",
+    description: "A videogame in windows console based in the Knight's Tour mechanics. The knight visits every square at least once with out returning a second time. The game is lost when no possible movements are available. It is won when avery square of the board has been visited.",
+    technologies: ["C#"],
 });
 
 export function ProjectSections(props) {
