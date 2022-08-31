2970f8b5c2c90cf5444f90df39e860c67ac02a67
Samy <SamyIbrizPelaez.Dev@gmail.com>
2022-08-31 12:20:40 +0200

    Update atmosFragment.glsl

--- public/shaders/atmosFragment.glsl
+++ public/shaders/atmosFragment.glsl
@@ -3,7 +3,7 @@ varying vec3 vNormal;
 void main( void ) 
 {
 
-    float intensity = pow(0.4- dot(vNormal, vec3(0.0,0.0,1.0)),  5.0);
+    float intensity = pow(0.5- dot(vNormal, vec3(0.0,0.0,1.0)),  5.0);
 
     vec4 earthBlue = vec4(0.156,0.4603,0.6943,1.0);
 
